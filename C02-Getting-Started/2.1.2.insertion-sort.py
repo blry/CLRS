65874943ff3213b7f263fa877ca46696680ebb32
Alexander Sterpu <alex@finalsoft.ru>
2017-02-20 01:48:04 +0200

    ok

---
+++ C02-Getting-Started/2.1.2.insertion-sort.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+def insertionSort(A):
+    for i in range(1, len(A)):
+        k = A[i]
+        j = i - 1
+        while(j >= 0 and A[j] > k):
+            A[j + 1] = A[j]
+            j -= 1
+        A[j + 1] = k
+
+    return A
+
+A = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 100, -100, 100, -100, -1000]
+
+print(insertionSort(A))
