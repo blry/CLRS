c5b8aec89320ae3560f7293c1b5c634d095d1116
Alexander Sterpu <alex@finalsoft.ru>
2017-02-20 06:08:26 +0200

    ok

---
+++ C02-Getting-Started/2.1.4.sum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+def sum(A, B):
+    C = []
+    j = 0
+    
+    for i in range(len(A) - 1, -1, -1):
+        j = A[i] + B[i] + j
+        C.insert(0, j % 2)
+        j = j / 2
+
+    C.insert(0, j % 2)
+    
+    return C
+
+A = [1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1]
+B = [1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0]
+
+print(sum(A, B))
