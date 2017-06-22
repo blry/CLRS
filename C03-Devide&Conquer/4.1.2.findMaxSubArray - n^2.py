12094ebc53e9a04a0c990796087c414f22859075
Alexander Sterpu <alex@finalsoft.ru>
2017-06-22 21:20:29 +0300

    ok

---
+++ C03-Devide&Conquer/4.1.2.findMaxSubArray - n^2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def findMaxSubArray(A):
+    maxSum = low = high = 0
+    for l in range(0, len(A)):
+        current = 0
+        for r in range(l, len(A)):
+            current += A[r]
+            if current > maxSum:
+                maxSum = current
+                low = l
+                high = r
+    return maxSum, low, high
+
+A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7] 
+
+
+b, i, j = findMaxSubArray(A)
+print(b, [i, j], A[i:j + 1])
---
+++ C03-Devide&Conquer/4.1.3.findMaxSubArray-optimizedDevide.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def findMaxSubArrayLinear(A, low, high):
+    maxSum = maxLow = maxHigh = 0
+    for l in range(low, high + 1):
+        current = 0
+        for r in range(l, high + 1):
+            current += A[r]
+            if current > maxSum:
+                maxSum = current
+                maxLow = l
+                maxHigh = r
+
+    return maxSum, maxLow, maxHigh
+
+def findMaxSubArray(A, low, high):
+    if (high - low) < 13:
+        return findMaxSubArrayLinear(A, low, high)
+    else:
+        mid = (high + low) // 2
+        
+        leftSum, leftLow, leftHigh = findMaxSubArray(A, low, mid)
+        rightSum, rightLow, rightHigh = findMaxSubArray(A, mid + 1, high)
+        crossSum, crossLow, crossHigh = findMaxCrossMidSubArray(A, low, mid, high)
+
+        if (leftSum >= rightSum) and (leftSum >= crossSum):
+            return leftSum, leftLow, leftHigh
+        elif (rightSum >= leftSum) and (rightSum >= crossSum):
+            return rightSum, rightLow, rightHigh
+        else:
+            return crossSum, crossLow, crossHigh
+
+def findMaxCrossMidSubArray(A, low, mid, high):
+    maxLeftSum = current = A[mid]
+    leftLow = mid
+
+    for i in range(mid - 1, low - 1, -1):
+        current += A[i]
+        if current > maxLeftSum:
+            maxLeftSum = current
+            leftLow = i
+        
+    maxRightSum = current = A[mid + 1]
+    rightHigh = mid + 1
+
+    for i in range(mid + 2, high):
+        current += A[i]
+        if current > maxRightSum:
+            maxRightSum = current
+            rightHigh = i
+
+    return maxRightSum + maxLeftSum, leftLow, rightHigh
+
+A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7] * 10
+b, i, j = findMaxSubArray(A, 0, len(A) - 1)
+
+print(b, [i, j], A[i:j + 1])
